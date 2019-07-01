118210da272ae1423c5a85b4378366a602436a25
unknown <koushith97@gmail.com>
2019-07-01 15:22:20 +0530

    files added

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="footer bg-dark text-white mt-5 p-4 text-center">
+      Made with <i className="fas fa-heart" /> and <i className="fas fa-code" />{" "}
+      by{" "}
+      <a
+        href="https://www.koushith.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Koushith_Amin
+      </a>
+    </footer>
+  );
+};
